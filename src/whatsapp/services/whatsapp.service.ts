b22c71926d93373dba69d55c0a9a999511827211
Leandro Rocha <leormt4@gmail.com>
2024-01-24 11:37:39 -0300

    :sparkles: feat: Junte-se a um grupo utilizando código de convite

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -84,6 +84,7 @@ import {
   WhatsAppNumberDto,
 } from '../dto/chat.dto';
 import {
+  AcceptGroupInvite,
   CreateGroupDto,
   GetParticipant,
   GroupDescriptionDto,
@@ -3211,6 +3212,16 @@ export class WAStartupService {
       return { send: true, inviteUrl };
     } catch (error) {
       throw new NotFoundException('No send invite');
+    }
+  }
+
+  public async acceptInviteCode(id: AcceptGroupInvite) {
+    this.logger.verbose('Joining the group by invitation code: ' + id.inviteCode);
+    try {
+      const groupJid = await this.client.groupAcceptInvite(id.inviteCode);
+      return { accepted: true, groupJid: groupJid };
+    } catch (error) {
+      throw new NotFoundException('Accept invite error', error.toString());
     }
   }
 
